8666a5d22946ba792ce9291cd52ce78cbe0e9665
jacuervon <jacuervon@outlook.com>
2024-01-14 00:49:25 -0300

    Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string userInput;
+        int magicNumber;
+        int userGuessNumber;
+        bool isCorrect;
+
+        //Generate random number
+        Random randomGenerator = new Random();
+        magicNumber = randomGenerator.Next(1, 100);
+
+        // // //Prompt user for magic number
+        // Console.WriteLine("What is the magic number?: ");
+        // userInput = Console.ReadLine();
+        // magicNumber = int.Parse(userInput);
+
+        isCorrect = false;
+        while (!isCorrect)
+        {
+            //Guess the magic number
+            Console.WriteLine("Guess the magic number: ");
+            userInput = Console.ReadLine();
+            userGuessNumber = int.Parse(userInput);
+
+            //Check if user guess is correct
+            if (userGuessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                isCorrect = true;
+            }
+
+        }
     }
 }
