daeedfead4998332505926de25786e2ba86fbed7
jacuervon <jacuervon@outlook.com>
2024-01-14 00:31:14 -0300

    Learning Activity - C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string userInput;
+        string gradeLetter;
+        string gradeSign;
+        int gradePercentage;
+        bool isPassing;
+
+        // Prompt user for grade percentage
+        Console.WriteLine("What is your grade percentage?: ");
+        userInput = Console.ReadLine();
+        gradePercentage = int.Parse(userInput);
+
+        // Compute grade letter
+        if (gradePercentage >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        // Compute grade sign
+        int result = gradePercentage % 10;
+
+        if (result >= 7 && (gradeLetter != "F" || gradeLetter != "A"))
+        {
+            gradeSign = "+";
+        }
+        else if (result < 3 && gradeLetter != "F")
+        {
+            gradeSign = "-";
+        }else{
+            gradeSign = "";
+        }
+
+        // Compute if passing
+        if (gradePercentage >= 70)
+        {
+            isPassing = true;
+        }
+        else
+        {
+            isPassing = false;
+        }
+
+        Console.WriteLine($"your grade is {gradeLetter}{gradeSign}.");
+        Console.WriteLine($"{(isPassing ? "You passed!" : "Better luck next time!")}");
     }
 }
