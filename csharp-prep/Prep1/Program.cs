50a812d96ba7877ca7070fe08b20c4be5d5f168d
jacuervon <jacuervon@outlook.com>
2024-01-13 23:35:52 -0300

    Learning Activity - C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string first_name;
+        string last_name;
+        Console.WriteLine("What is your first name?");
+        first_name = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
+
     }
 }
