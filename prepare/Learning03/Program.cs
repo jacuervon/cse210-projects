bf0eb852ff592795665235fdb63eba92a4040390
jacuervon <jacuervon@outlook.com>
2024-01-25 23:10:28 -0300

    Learning Activity Week 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine($"{f1.GetFractionString()}");
+        Console.WriteLine($"{f1.GetFractionValue()}");
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine($"{f2.GetFractionString()}");
+        Console.WriteLine($"{f2.GetFractionValue()}");
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine($"{f3.GetFractionString()}");
+        Console.WriteLine($"{f3.GetFractionValue()}");
+
+        f3.SetTop(1);
+        f3.SetBottom(3);
+        Console.WriteLine($"{f3.GetFractionString()}");
+        Console.WriteLine($"{f3.GetFractionValue()}");
     }
 }
